dbbf92ec6d658b1371b6eff8f24a211e4cbe02b9
willy <guillermoaldaz@guillermos-MacBook-Pro.local>
2021-08-24 20:07:14 -0700

    added content and functionality to Create Account component

--- createaccount.js
+++ createaccount.js
@@ -1,5 +1,66 @@
 function CreateAccount(){
+    const [show, setShow]     = React.useState(true);
+    const [status, setStatus] = React.useState('');
+
     return (
-        <h1>Create Account</h1>
+        <Card
+            bgcolor="primary"
+            header="Create Account"
+            status={status}
+            body={show ?
+                <CreateForm setShow={setShow}/> :
+                <CreateMsg setShow={setShow}/>}
+        />
     )
+}
+
+function CreateMsg(props) {
+    return(<>
+            <h5>Success</h5>
+            <button type="submit" 
+                className="btn btn-light" 
+                onClick={() => props.setShow(true)}>Add another account</button>
+        </>)
+}
+
+function CreateForm(props) {
+    const [name, setName]         = React.useStat('');
+    const [email, setEmail]       = React.useState('');
+    const [password, setPassword] = React.useState('');
+    const ctx = React.useContext(UserContext);
+
+    function handle() {
+        console.log(name, email, password);
+        ctx.user.push({name, email, password});
+        props.setShow(false);
+    }
+
+    return (<>
+
+        Name<br/>
+        <input type="input"
+            className="form-control"
+            placeholder="Enter name"
+            value={name}
+            onChange={e => setName(e.currenttarget.valeu)} /><br/>
+        
+        Email address<br/>
+        <input type="input"
+            className="form-control"
+            placeholder="Enter email"
+            value={email}
+            onChange={e => setEmail(e.currentTarget.value)} /><br/>
+        
+        Password<br/>
+        <input type="input"
+            className="form-control"
+            placeholder="Enter password"
+            value={password}
+            onChange={e => setPassword(e.currentTarget.value)} /><br/>
+        
+        <button type="submit"
+            className="btn btn-light"
+            onClick={handle}>Create Account</button>
+        
+    </>);
 }
