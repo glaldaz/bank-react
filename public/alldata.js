b591bdc8524790b18b454fa7900338c6a1aa6d1d
willy <guillermoaldaz@guillermos-MacBook-Pro.local>
2021-08-24 22:45:52 -0700

    added functionality to the font end to use the express routs to show all data in the database.

--- public/alldata.js
+++ public/alldata.js
@@ -1,9 +1,18 @@
 function AllData(){
-    const ctx = React.useContext(UserContext);
-    return (
-        <>
+    const [data, setData] = React.useState('');
+
+    React.useEffect(() => {
+        // fetch all accounts from API
+        fetch('/account/all')
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                setData(JSON.stringify(data));
+            });
+    }, []);
+
+    return (<>
             <h1>All Data</h1>
-            {JSON.stringify(ctx)}
-        </>
-    )
+            {data}
+        </>);
 }
