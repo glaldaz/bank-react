6b500faa70d21802068e17cd26ad7716be1f865d
willy <guillermoaldaz@guillermos-MacBook-Pro.local>
2021-08-24 21:52:28 -0700

    file changes

--- alldata.js
+++
@@ -1,9 +0,0 @@
-function AllData(){
-    const ctx = React.useContext(UserContext);
-    return (
-        <>
-            <h1>All Data</h1>
-            {JSON.stringify(ctx)}
-        </>
-    )
-}
