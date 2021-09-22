0bafd9699e345b53afec69402fa52dda8a28008d
Willy <glaldaz@gmail.com>
2021-09-21 17:00:45 -0700

    refactored code to move the create for function to the context.js file to be used by other elements.

--- public/context.js
+++ public/context.js
@@ -4,11 +4,13 @@ const HashRouter  = ReactRouterDOM.HashRouter;
 const UserContext = React.createContext(null);
 
 function Card(props){
+
     function classes(){
         const bg = props.bgcolor ? ' bg-' + props.bgcolor: ' ';
         const txt = props.txtcolor ? ' text' + props.txtcolor: ' text-white';
         return 'card mb-3 ' + bg + txt;
     }
+    const [show, setShow] = props.show;
 
     return (
         <div className={classes()} style={{maxwidth: "18rem"}}>
@@ -16,9 +18,74 @@ function Card(props){
             <div className="card-body">
                 {props.title && (<h5 className="card-title">{props.title}</h5>)}
                 {props.text && (<p className="card-text">{props.txt}</p>)}
-                {props.body}
+                {show ? <CreateForm
+                    name = {props.name}
+                    email={props.email}
+                    password={props.password}
+                    showSubmit={props.showSubmit}
+                    handle={props.handle}
+                ></CreateForm> :
+                <CreateMsg
+                    setShow={setShow}
+                ></CreateMsg>}
                 {props.status && (<div id='createStatus'>{props.status}</div>)}
             </div>
         </div>
     );
+}
+
+function CreateMsg(props) {
+    return(<>
+            <h5>Success</h5>
+            <button type="submit" 
+                className="btn btn-light" 
+                onClick={() => props.setShow(true)}>Add another account</button>
+        </>)
+}
+
+function CreateForm(props) {
+    const [name, setName, showName] = props.name;//[name, setName]         = React.useState('');
+    const [email, setEmail, showEmail]= props.email;//[email, setEmail]       = React.useState('');
+    const [password, setPassword, showPassword] = props.password;//[password, setPassword] = React.useState('');
+    const showSubmit = props.showSubmit;
+
+    return (<>
+        {showName &&
+        <div>
+            Name<br/>
+            <input type="input"
+                className="form-control"
+                placeholder="Enter name"
+                value={name}
+                onChange={e => setName(e.currentTarget.value)} /><br/>
+        </div>}
+        
+        {showEmail &&
+        <div>
+            Email address<br/>
+            <input type="input"
+                className="form-control"
+                placeholder="Enter email"
+                value={email}
+                onChange={e => setEmail(e.currentTarget.value)} /><br/>
+        </div>}
+        
+        {showPassword &&
+        <div>
+            Password<br/>
+            <input type="input"
+                className="form-control"
+                placeholder="Enter password"
+                value={password}
+                onChange={e => setPassword(e.currentTarget.value)} /><br/>
+        </div>}
+        
+
+        {showSubmit &&
+        <div>
+            <button type="submit"
+                className="btn btn-light"
+                onClick={props.handle}>Create Account</button>
+        </div>}
+    </>);
 }
--- public/createaccount.js
+++ public/createaccount.js
@@ -1,66 +1,31 @@
 function CreateAccount(){
-    const [show, setShow]     = React.useState(true);
-    const [status, setStatus] = React.useState('');
+    const [name, setName]         = React.useState('');
+    const [email, setEmail]       = React.useState('');
+    const [password, setPassword] = React.useState('');
+    const [show, setShow]         = React.useState(true);
+    const ctx = React.useContext(UserContext);
+
+    const handle = function() {
+        console.log(name, email, password);
+        const url = `/account/create/${name}/${email}/${password}`;
+        (async () => {
+            var res = await fetch(url);
+            var data = await res.json();
+            console.log(data);
+        })();
+        setShow(false);
+    }
 
     return (
         <Card
             bgcolor="primary"
             header="Create Account"
-            status={status}
-            body={show ?
-                <CreateForm setShow={setShow}/> :
-                <CreateMsg setShow={setShow}/>}
+            name = {[name, setName, true]}
+            email={[email, setEmail, true]}
+            password={[password, setPassword, true]}
+            show={[show, setShow]}
+            showSubmit={true}
+            handle={handle}
         />
     )
-}
-
-function CreateMsg(props) {
-    return(<>
-            <h5>Success</h5>
-            <button type="submit" 
-                className="btn btn-light" 
-                onClick={() => props.setShow(true)}>Add another account</button>
-        </>)
-}
-
-function CreateForm(props) {
-    const [name, setName]         = React.useState('');
-    const [email, setEmail]       = React.useState('');
-    const [password, setPassword] = React.useState('');
-    const ctx = React.useContext(UserContext);
-
-    function handle() {
-        console.log(name, email, password);
-        ctx.users.push({name, email, password});
-        props.setShow(false);
-    }
-
-    return (<>
-
-        Name<br/>
-        <input type="input"
-            className="form-control"
-            placeholder="Enter name"
-            value={name}
-            onChange={e => setName(e.currentTarget.value)} /><br/>
-        
-        Email address<br/>
-        <input type="input"
-            className="form-control"
-            placeholder="Enter email"
-            value={email}
-            onChange={e => setEmail(e.currentTarget.value)} /><br/>
-        
-        Password<br/>
-        <input type="input"
-            className="form-control"
-            placeholder="Enter password"
-            value={password}
-            onChange={e => setPassword(e.currentTarget.value)} /><br/>
-        
-        <button type="submit"
-            className="btn btn-light"
-            onClick={handle}>Create Account</button>
-        
-    </>);
 }
